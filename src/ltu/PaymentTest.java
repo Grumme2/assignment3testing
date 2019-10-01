a7a44913fb7125a1f56b6189586ec48de916dd02
drakcir97 <richol-6@student.ltu.se>
2019-10-01 13:54:16 +0200

    Added tests for study pace and ratio

--- src/ltu/PaymentTest.java
+++ src/ltu/PaymentTest.java
@@ -29,4 +29,46 @@ public class PaymentTest
         payment = new PaymentImpl(getCalendar());
         assertEquals(9904,payment.getMonthlyAmount("19970606-7973", 19900, 100, 100));
     }
+
+    @Test
+    public void getstudyamounttest() throws IOException {
+        PaymentImpl test = new PaymentImpl(getCalendar());
+        int testnotime = test.getMonthlyAmount("19970606-7973",0,0,100);
+        int testhalftime = test.getMonthlyAmount("19970606-7973",0,50,100);
+        int testalmostfulltime = test.getMonthlyAmount("19970606-7973",0,99,100);
+        int testfulltime = test.getMonthlyAmount("19970606-7973",0,100,100);
+        assertEquals(0,testnotime);
+        assertEquals(1396,testhalftime-3564);
+        assertEquals(1396,testalmostfulltime-3564);
+        assertEquals(2816,testfulltime-7088);
+    }
+
+    @Test
+    public void getstudyratiotest() throws IOException {
+        PaymentImpl test = new PaymentImpl(getCalendar());
+
+        int testfull_noratio = test.getMonthlyAmount("19970606-7973",0,100,0);
+        int testhalf_noratio = test.getMonthlyAmount("19970606-7973",0,50,0);
+        int testno_noratio = test.getMonthlyAmount("19970606-7973",0,0,0);
+
+        int testfull_halfratio = test.getMonthlyAmount("19970606-7973",0,100,50);
+        int testhalf_halfratio = test.getMonthlyAmount("19970606-7973",0,50,50);
+        int testno_halfratio = test.getMonthlyAmount("19970606-7973",0,0,50);
+
+        int testfull_fullratio = test.getMonthlyAmount("19970606-7973",0,100,100);        
+        int testhalf_fullratio = test.getMonthlyAmount("19970606-7973",0,50,100);      
+        int testno_fullratio = test.getMonthlyAmount("19970606-7973",0,0,100);   
+
+        assertEquals(0,testfull_noratio);
+        assertEquals(0,testhalf_noratio);
+        assertEquals(0,testno_noratio);
+
+        assertEquals(7088+2816,testfull_halfratio);
+        assertEquals(3564+1396,testhalf_halfratio);
+        assertEquals(0,testno_halfratio);
+
+        assertEquals(7088+2816,testfull_fullratio);
+        assertEquals(3564+1396,testhalf_fullratio);
+        assertEquals(0,testno_fullratio);
+    }
 }
