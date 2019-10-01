980419e2b8097a019ed11bb68d74b0a9634f7629
Grumme2 <agh@live.se>
2019-10-01 12:39:44 +0200

    first commit

---
+++ src/ltu/PaymentTest.java
@@ -0,0 +1,32 @@
+package ltu;
+import static ltu.CalendarFactory.getCalendar;
+import static org.junit.Assert.*;
+
+import java.beans.Transient;
+import java.io.IOException;
+
+import org.junit.Test;
+
+public class PaymentTest
+{
+    @Test
+    public void testSilly()
+    {
+        assertEquals(1, 1);
+        assertEquals(4, 4);
+        
+        
+    }
+    @Test
+    public void testsilly2(){
+        assertEquals(1, 1);
+    }
+
+    @Test
+    public void getpaymentamounttest() throws IOException{
+        
+        PaymentImpl payment;
+        payment = new PaymentImpl(getCalendar());
+        assertEquals(9904,payment.getMonthlyAmount("19970606-7973", 19900, 100, 100));
+    }
+}
